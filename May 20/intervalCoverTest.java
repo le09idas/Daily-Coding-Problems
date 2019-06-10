0d2ab243a31de34e533a62e986e47be94dd207b7
Nicholas Petriello <nicholaspetriello@gmail.com>
2019-06-10 13:15:12 -0400

    Worked Some More on DCP May 20
    Still having trouble getting the list to get ordered correctly

--- May 20/intervalCoverTest.java
+++ May 20/intervalCoverTest.java
@@ -1,124 +1,126 @@
+import java.util.ArrayList;
+
 //intervalCoverTest
-class Set
-{
-	ArrayList<int> set;
-	
-	public Set()
-	{
-		this.set = new ArrayList<int>();
-	}
-	
-	public void addElement(int e)
-	{
-		this.set.addItem(e);
-	}
-	
-	public String toString()
-	{
-		String displaySet = 
-		for(int e : this.set)
-		{
-			
-		}
-	}
+class Set {
+    ArrayList<String> set;
+
+    public Set() {
+        this.set = new ArrayList<String>();
+    }
+
+    public void addElement(int e) {
+        this.set.add(Integer.toString(e));
+    }
+
+    public String toString() {
+        String displaySet = "{";
+
+        int i = 0;
+
+        displaySet = displaySet.concat(this.set.get(i));
+        i++;
+
+        while (i < this.set.size()) {
+            displaySet = displaySet.concat(", " + this.set.get(i));
+            i++;
+        }
+
+        displaySet = displaySet.concat("}");
+
+        return displaySet;
+    }
 }
 
-class Node 
-{
+class Node {
     int lowLim;
     int highLim;
 
     Node prev;
     Node next;
 
-    public Node(int low, int high) 
-    {
+    public Node(int low, int high) {
         this.lowLim = low;
         this.highLim = high;
         this.prev = null;
         this.next = null;
     }
 
-    public String toString() 
-    {
+    public String toString() {
         return "[" + Integer.toString(this.lowLim) + "," + Integer.toString(this.highLim) + "]";
     }
 }// end Node class
 
-class List 
-{
+class List {
     Node head;
     int lowExtreme;
     int highExtreme;
 
-    public List(Node head) 
-    {
+    public List(Node head) {
         this.head = head;
         this.lowExtreme = head.lowLim;
         this.highExtreme = head.highLim;
     }
 
-    public void addInterval(Node interval) 
-    {
+    public void addInterval(Node interval) {
         Node ptr = this.head;
-        while (true) 
-        {
-            if (interval.lowLim < ptr.lowLim && interval.highLim <= ptr.lowLim) 
-            {
-                if (ptr.prev == null) 
-                {
+
+        while (ptr != null) {
+            if (ptr.prev == null) {
+                if (interval.lowLim < ptr.lowLim) {
                     this.head = interval;
                     this.head.next = ptr;
-                    this.lowExtreme = this.head.lowLim;
-                } 
-                else 
-                {
+                    ptr.prev = this.head;
+                }
+                if (interval.lowLim == ptr.lowLim && interval.highLim < ptr.highLim) {
+                    this.head = interval;
+                    this.head.next = ptr;
+                    ptr.prev = this.head;
+                }
+                if (interval.lowLim == ptr.lowLim && interval.highLim > ptr.highLim) {
                     interval.prev = ptr;
                     interval.next = ptr.next;
-                    ptr.next = interval;
+                    if (ptr.next == null) {
+                        ;
+                    } else {
+                        ptr.next.prev = interval;
+                    }
                 }
                 return;
-            }
-            if (interval.lowLim == ptr.lowLim && interval.highLim <= ptr.highLim) 
-            {
-                interval.prev = ptr.prev;
-                interval.prev.next = interval;
-                interval.next = ptr;
-                ptr.prev = interval;
-                return;
-            }
-            if (interval.lowLim == ptr.lowLim && interval.highLim >= ptr.highLim) 
-            {
-                interval.next = ptr.next;
-                ptr.next = interval;
-                interval.prev = ptr;
-                return;
-            }
-            if (ptr.next == null) 
-            {
-                ptr.next = interval;
-                interval.prev = ptr;
-                if (interval.highLim >= ptr.highLim)
-                    this.highExtreme = interval.highLim;
-                return;
+            } else {
+                if (interval.lowLim <= ptr.lowLim) {
+                    if (interval.highLim <= ptr.highLim) {
+                        interval.prev = ptr.prev;
+                        interval.next = ptr;
+                        ptr.prev.next = interval;
+                        ptr.prev = interval;
+                    } else {
+                        interval.prev = ptr;
+                        interval.next = ptr.next;
+                        ptr.next.prev = interval;
+                        ptr.next = interval;
+                    }
+                    return;
+                }
+
             }
             ptr = ptr.next;
+
         }
 
     }
-    
-    public Set findSmallestCover(List intervals)
-    {
-    	
-    }
-}//end class List
 
-class TestApp 
-{
-    public static void main(String[] args) 
-    {
-        Node one = new Node(1, 2);
-        Node two = new Node(2, 2);
+    public Set findSmallestCover(List intervals) {
+
+        Set smallest = new Set();
+
+        return smallest;
+    }
+}// end class List
+
+class TestApp {
+    public static void main(String[] args) {
+        Node one = new Node(2, 2);
+        Node two = new Node(1, 2);
         Node three = new Node(2, 1);
         Node four = new Node(1, 3);
 
@@ -128,12 +130,18 @@ class TestApp
         list.addInterval(four);
 
         Node curr = list.head;
-        while (curr != null) 
-        {
+        while (curr != null) {
             System.out.println(curr.toString());
             curr = curr.next;
         }
+
+        System.out.println("LowExtreme : " + list.lowExtreme);
+        System.out.println("HighExtreme : " + list.highExtreme);
+
+        Set s = new Set();
+        s.addElement(1);
+        s.addElement(2);
+        System.out.println(s.toString());
     }
 
 }// end TestApp
-    
