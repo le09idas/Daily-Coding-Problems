aecde87eaf011e6a2b2f69f3e433cffe7d18b19c
Nicholas Petriello <nicholaspetriello@gmail.com>
2019-07-09 23:01:45 -0400

    Made Progress on DCP July 9
    Got the idea of how to go about sorting the array but the logic needs to be reworked.

--- 2019/July/July 9/orderLinkedListTest.java
+++ 2019/July/July 9/orderLinkedListTest.java
@@ -17,6 +17,90 @@ class Node
     }
 }
 
+class NodeSorter
+{
+    public static Node mergeSort(Node start, int seg)
+    {
+        if(seg == 1)
+        {
+            start.next = null;
+            return start;
+        }
+        else if(seg == 2)
+        {
+            if(start.value <= start.next.value)
+            {
+                start.next = null;
+                return start;
+            }
+            else
+            {
+                Node temp = start.next;
+                start.next = null;
+                temp.next = start;
+                return temp;
+            }
+        }
+        else
+        {
+            int piv = 0;
+            Node ptr = start;
+
+            while(piv <= seg/2)
+            {
+                ptr = ptr.next;
+                piv++;
+            }
+
+            Node sublist1 = mergeSort(start, seg/2 + seg%2);
+            Node sublist2 = mergeSort(ptr.next, seg - seg/2 - seg%2);
+
+
+            Node fuser = null;
+            Node fused = null;
+
+            if(sublist1.value <= sublist2.value)
+            {
+                fused = sublist1;
+                sublist1 = sublist1.next;
+            }
+            else
+            {
+                fused = sublist2;
+                sublist2 = sublist2.next;
+            }
+
+            fuser = fused;
+
+            while(sublist1 != null && sublist2 != null)
+            {
+                if(sublist1.value <= sublist2.value)
+                {
+                    fuser.next = sublist1;
+                    sublist1 = sublist1.next;
+                }
+                else
+                {
+                    fuser.next = sublist2;
+                    sublist2 = sublist2.next;
+                }
+                fuser = fuser.next;
+            }
+
+            if(sublist1 != null)
+            {
+                fuser.next = sublist1;
+            }
+            else if(sublist2 != null)
+            {
+                fuser.next = sublist2;
+            }
+
+            return fused;
+        }
+    }
+}
+
 class TestApp
 {
     public static void main(String[] args)
@@ -24,20 +108,43 @@ class TestApp
         Node one = new Node(4);
         Node two = new Node(1);
         Node three = new Node(-3);
-        Node four = new Node(99);
+        //Node four = new Node(99);
+        //Node five = new Node(5);
+        //Node six = new Node(6);
 
         one.next = two;
         two.next = three;
-        three.next = four;
+        //three.next = four;
+        //four.next = five;
+        //five.next = six;
 
         Node ptr = one;
+
+        System.out.println("Initial list;");
+
         while(ptr!=null)
         {
             System.out.print(ptr.toString());
             if(ptr.next != null)
             {
-                System.out.print(print("->"));
+                System.out.print("->");
             }
+            ptr = ptr.next;
+        }
+        System.out.println("");
+
+        Node sorted = NodeSorter.mergeSort(one, 3);
+
+        System.out.println("Sorted list:");
+
+        while(sorted!=null)
+        {
+            System.out.print(sorted.toString());
+            if(sorted.next != null)
+            {
+                System.out.print("->");
+            }
+            sorted = sorted.next;
         }
         System.out.println("");
     }
